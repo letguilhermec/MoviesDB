6b015fd4d9c7ac51d8488303fc7b12e5eedc5aac
Guilherme Correa <correacgui@gmail.com>
2023-05-16 14:24:06 -0300

    Finishes BackdropCard Carousel prototype

--- imdb_project/Views/MovieBackdropCarouselView.swift
+++ imdb_project/Views/MovieBackdropCarouselView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct MovieBackdropCarouselView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  let title: String
+  let movies: [Movie]
+  var body: some View {
+    VStack(alignment: .leading, spacing: 0) {
+      Text(title)
+        .font(.title)
+        .fontWeight(.bold)
+        .padding(.horizontal)
+      
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack(alignment: .top, spacing: 16) {
+          ForEach(self.movies) { movie in
+            MovieBackdropCard(movie: movie)
+              .frame(width: 272, height: 200)
+              .padding(.leading, movie.id == self.movies.first!.id ? 16 : 0)
+              .padding(.trailing, movie.id == self.movies.first!.id ? 16 : 0)
+          }
+        }
+      }
     }
+  }
 }
 
 struct MovieBackdropCarouselView_Previews: PreviewProvider {
-    static var previews: some View {
-        MovieBackdropCarouselView()
-    }
+  static var previews: some View {
+    MovieBackdropCarouselView(title: "Latest", movies: Movie.stubbedMovies)
+  }
 }
