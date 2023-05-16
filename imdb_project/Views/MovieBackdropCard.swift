b22df103bf70615a31947b33864ef6b6f867e628
Guilherme Correa <correacgui@gmail.com>
2023-05-16 14:07:51 -0300

    Starts Backdrop Image and Creates ImageLoader

---
+++ imdb_project/Views/MovieBackdropCard.swift
@@ -0,0 +1,20 @@
+//
+//  MovieBackdropCard.swift
+//  imdb_project
+//
+//  Created by Guilherme de Carvalho Correa on 16/05/23.
+//
+
+import SwiftUI
+
+struct MovieBackdropCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieBackdropCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieBackdropCard()
+    }
+}
