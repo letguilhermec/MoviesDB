197589f5681c3b87984875065525ec29f4fe7886
Guilherme Correa <correacgui@gmail.com>
2023-05-16 15:51:45 -0300

    Creates MovieDetailState as ObservableObj

---
+++ imdb_project/Views/MovieDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetailView.swift
+//  imdb_project
+//
+//  Created by Guilherme de Carvalho Correa on 16/05/23.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetailView()
+    }
+}
