70796d3845cbb688090e4d9a27ace9d067ee49ed
Guilherme Correa <correacgui@gmail.com>
2023-05-16 11:13:42 -0300

    Completes MovieService -> MovieError and protocol

--- imdb_project/Services/MovieService.swift
+++ imdb_project/Services/MovieService.swift
@@ -8,10 +8,12 @@
 import Foundation
 
 protocol MovieService {
-  
+  func fetchMovies(from endpoint: MovieListEndpoint, completion: @escaping (Result<MovieResponse, MovieError>) -> ())
+  func fetchMovie(id: Int, completion: @escaping (Result<Movie, MovieError>) -> ())
+  func searchMovie(query: String, completion: @escaping (Result<MovieResponse, MovieError>) -> ())
 }
 
-enum MovieListEndpoint: String {
+enum MovieListEndpoint: String, CaseIterable {
   case nowPlaying = "now_playing"
   case upcoming
   case topRated = "top_rated"
@@ -19,7 +21,32 @@ enum MovieListEndpoint: String {
   
   var description: String {
     switch self {
-      case .nowPlaying return "Now Playing"
+    case .nowPlaying: return "Now Playing"
+    case .upcoming: return "Upcoming"
+    case .topRated: return "Top Rated"
+    case .popular: return "Popular"
     }
+  }
+}
+
+enum MovieError: Error, CustomNSError {
+  case apiError
+  case invalidEndpoint
+  case invalidResponse
+  case noData
+  case serializationError
+  
+  var localizedDescription: String {
+    switch self {
+    case .apiError: return "Failed to fetch data"
+    case .invalidEndpoint: return "Invalid endpoint"
+    case .invalidResponse: return "Invalid response"
+    case .noData: return "No data"
+    case .serializationError: return "Failed to decode data"
+    }
+  }
+  
+  var errorUserInfo: [String : Any] {
+    [NSLocalizedDescriptionKey: localizedDescription]
   }
 }
