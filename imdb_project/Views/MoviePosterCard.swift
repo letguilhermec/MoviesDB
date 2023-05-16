e345e9fbfcf6ce5ffb5b05c5e3eefc903059b3c6
Guilherme Correa <correacgui@gmail.com>
2023-05-16 14:34:56 -0300

    Finishes PosterCard prototype

--- imdb_project/Views/MoviePosterCard.swift
+++ imdb_project/Views/MoviePosterCard.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct MoviePosterCard: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  let movie: Movie
+  @ObservedObject var imageLoader = ImageLoader()
+  
+  var body: some View {
+    ZStack {
+      if self.imageLoader.image != nil {
+        Image(uiImage: self.imageLoader.image!)
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .cornerRadius(8)
+          .shadow(radius: 4)
+      } else {
+        Rectangle()
+          .fill(Color.gray.opacity(0.3))
+          .cornerRadius(8)
+          .shadow(radius: 4)
+        
+        Text(movie.title)
+          .multilineTextAlignment(.center)
+      }
     }
+    .frame(width: 204, height: 306)
+    .onAppear {
+      self.imageLoader.loadImage(with: self.movie.posterURL)
+    }
+  }
 }
 
 struct MoviePosterCard_Previews: PreviewProvider {
-    static var previews: some View {
-        MoviePosterCard()
-    }
+  static var previews: some View {
+    MoviePosterCard(movie: Movie.stubbedMovie)
+  }
 }
