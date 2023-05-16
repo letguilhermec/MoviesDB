397b8ff09af8f58099353457fe754130166b5a37
Guilherme Correa <correacgui@gmail.com>
2023-05-16 13:56:02 -0300

    Adds Resources files and extension to Movies
    - Bootstrap project w/ mocked responses

---
+++ imdb_project/Models/Movie+Stub.swift
@@ -0,0 +1,33 @@
+//
+//  Movie+Stub.swift
+//  imdb_project
+//
+//  Created by Guilherme de Carvalho Correa on 16/05/23.
+//
+
+import Foundation
+
+extension Movie {
+  static var stubbedMovies: [Movie] {
+    let response: MovieResponse? = try Bundle.main.loadAndDecodeJSON(filename: "movie_list")
+    return response!.results
+  }
+  
+  static var stubbedMovie: Movie {
+    stubbedMovies[0]
+  }
+}
+
+extension Bundle {
+  func loadAndDecodeJSON<D: Decodable>(filename: String) throws -> D? {
+    do {
+      guard let url = self.url(forResource: filename, withExtension: "json") else {
+        return nil
+      }
+      let data = try Data(contentsOf: url)
+      let jsonDecoder = Utils.jsonDecoder
+      let decodedModel = try jsonDecoder.decode(D.self, from: data)
+      return decodedModel
+    }
+  }
+}
