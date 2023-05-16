bae51f06fef756f8995e0fa5995f91adcc6bed5c
Guilherme Correa <correacgui@gmail.com>
2023-05-16 15:12:38 -0300

    Finishes MovieListView prototype
    - Still lacking API Key for testing; API website malfunctioning

--- imdb_project/Views/MovieListView.swift
+++ imdb_project/Views/MovieListView.swift
@@ -8,13 +8,68 @@
 import SwiftUI
 
 struct MovieListView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @ObservedObject private var nowPlayingState = MovieListState()
+  @ObservedObject private var upcomingState = MovieListState()
+  @ObservedObject private var topRatedState = MovieListState()
+  @ObservedObject private var popularState = MovieListState()
+  
+  var body: some View {
+    NavigationView {
+      List {
+        Group {
+          if nowPlayingState.movies != nil {
+            MoviePosterCarouselView(title: "Now Playing", movies: nowPlayingState.movies!)
+          } else {
+            LoadingView(isLoading: self.nowPlayingState.isLoading, error: self.nowPlayingState.error) {
+              self.nowPlayingState.loadMovies(with: .nowPlaying)
+            }
+          }
+        }
+        
+        Group {
+          if upcomingState.movies != nil {
+            MovieBackdropCarouselView(title: "Upcoming", movies: upcomingState.movies!)
+          } else {
+            LoadingView(isLoading: self.upcomingState.isLoading, error: self.upcomingState.error) {
+              self.upcomingState.loadMovies(with: .upcoming)
+            }
+          }
+        }
+       
+        Group {
+          if topRatedState.movies != nil {
+            MovieBackdropCarouselView(title: "Top Rated", movies: topRatedState.movies!)
+          } else {
+            LoadingView(isLoading: self.topRatedState.isLoading, error: self.topRatedState.error) {
+              self.topRatedState.loadMovies(with: .topRated)
+            }
+          }
+        }
+        
+        Group {
+          if popularState.movies != nil {
+            MovieBackdropCarouselView(title: "Popular", movies: popularState.movies!)
+          } else {
+            LoadingView(isLoading: self.popularState.isLoading, error: self.popularState.error) {
+              self.popularState.loadMovies(with: .popular)
+            }
+          }
+        }
+        
+      }
+      .navigationBarTitle("The MovieDB")
     }
+    .onAppear {
+      self.nowPlayingState.loadMovies(with: .nowPlaying)
+      self.upcomingState.loadMovies(with: .upcoming)
+      self.topRatedState.loadMovies(with: .topRated)
+      self.popularState.loadMovies(with: .popular)
+    }
+  }
 }
 
 struct MovieListView_Previews: PreviewProvider {
-    static var previews: some View {
-        MovieListView()
-    }
+  static var previews: some View {
+    MovieListView()
+  }
 }
