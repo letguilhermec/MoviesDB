cf601371513e9e8dd57d032f08b7de167089e80a
Guilherme Correa <correacgui@gmail.com>
2023-05-16 14:40:27 -0300

    Fixes adding trailing padding to last only

--- imdb_project/Views/MovieBackdropCarouselView.swift
+++ imdb_project/Views/MovieBackdropCarouselView.swift
@@ -23,7 +23,7 @@ struct MovieBackdropCarouselView: View {
             MovieBackdropCard(movie: movie)
               .frame(width: 272, height: 200)
               .padding(.leading, movie.id == self.movies.first!.id ? 16 : 0)
-              .padding(.trailing, movie.id == self.movies.first!.id ? 16 : 0)
+              .padding(.trailing, movie.id == self.movies.last!.id ? 16 : 0)
           }
         }
       }
---
+++ imdb_project/Views/MoviePosterCarouselView.swift
@@ -0,0 +1,20 @@
+//
+//  MoviePosterCarouselView.swift
+//  imdb_project
+//
+//  Created by Guilherme de Carvalho Correa on 16/05/23.
+//
+
+import SwiftUI
+
+struct MoviePosterCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoviePosterCarouselView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoviePosterCarouselView()
+    }
+}
