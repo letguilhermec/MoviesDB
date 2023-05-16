22a6ccc0c7248b211b00877cb8b6fdaa8eeb21b0
Guilherme Correa <correacgui@gmail.com>
2023-05-16 15:00:43 -0300

    Starts Loading View...

---
+++ imdb_project/Views/LoadingView.swift
@@ -0,0 +1,50 @@
+//
+//  LoadingView.swift
+//  imdb_project
+//
+//  Created by Guilherme de Carvalho Correa on 16/05/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+  let isLoading: Bool
+  let error: NSError?
+  let retryAction: (() -> ())?
+  
+  var body: some View {
+    Group {
+      if isLoading {
+        HStack {
+          Spacer()
+          ActivityIndicatorView()
+          Spacer()
+        }
+      } else if error != nil {
+        HStack {
+          Spacer()
+          VStack(spacing: 4) {
+            Text(error!.localizedDescription)
+              .font(.headline)
+            if self.retryAction != nil {
+              Button(action: self.retryAction!) {
+                Text("Retry")
+              }
+              .foregroundColor(Color(UIColor.systemBlue))
+              .buttonStyle(PlainButtonStyle())
+            }
+          }
+          Spacer()
+        }
+      }
+      
+    
+    }
+  }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+  static var previews: some View {
+    LoadingView(isLoading: true, error: nil, retryAction: nil)
+  }
+}
