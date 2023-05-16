8593bd7dd88ea900e791d00ba3c62fad36acb4d9
Guilherme Correa <correacgui@gmail.com>
2023-05-16 10:04:58 -0300

    Initial Commit

---
+++ imdb_project/imdb_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  imdb_projectApp.swift
+//  imdb_project
+//
+//  Created by Guilherme de Carvalho Correa on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct imdb_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
