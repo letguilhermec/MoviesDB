5cb622818cae0d9b5a30be533d2a4a78f7e38a05
Guilherme Correa <correacgui@gmail.com>
2023-05-17 13:07:41 -0300

    Adds cast and crew to Movie Detail View

--- imdb_project/Views/MovieDetailView.swift
+++ imdb_project/Views/MovieDetailView.swift
@@ -53,9 +53,63 @@ struct MovieDetailListView: View {
         }
         Text(movie.scoreText)
       }
-        .listRowSeparator(.hidden)
+      .listRowSeparator(.hidden)
       Divider()
         .listRowSeparator(.hidden)
+      
+      HStack(alignment: .top, spacing: 4) {
+        if movie.cast != nil &&
+            movie.cast!.count > 0 {
+          VStack(alignment: .leading, spacing: 4) {
+            Text("Starring")
+              .font(.headline)
+            ForEach(self.movie.cast!.prefix(9)) { cast in
+              Text(cast.name)
+            }
+          }
+          .listRowSeparator(.hidden)
+          .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+          Spacer()
+        }
+        
+        if movie.crew != nil && movie.crew!.count > 0 {
+          VStack(alignment: .leading, spacing: 4) {
+            
+            if movie.directors != nil &&
+                movie.directors!.count > 0 {
+              Text("Director(s)")
+                .font(.headline)
+              ForEach(self.movie.directors!.prefix(2)) { crew in
+                Text(crew.name)
+              }
+            }
+            
+            if movie.producers != nil &&
+                movie.producers!.count > 0 {
+              Text("Producer(s)")
+                .font(.headline)
+                .padding(.top)
+              ForEach(self.movie.producers!.prefix(2)) { crew in
+                Text(crew.name)
+              }
+            }
+            
+            if movie.screenWriters != nil &&
+                movie.screenWriters!.count > 0 {
+              Text("Screenwriter(s)")
+                .font(.headline)
+                .padding(.top)
+              ForEach(self.movie.screenWriters!.prefix(2)) { crew in
+                Text(crew.name)
+              }
+            }
+          }
+          .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+        }
+      }
+      Divider()
+      .listRowSeparator(.hidden)
+      
     }
     .listStyle(.plain)
   }
