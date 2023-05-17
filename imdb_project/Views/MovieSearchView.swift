4866ad138e3d1d3ee9596a16b17d7e5a374435f6
Guilherme Correa <correacgui@gmail.com>
2023-05-17 14:42:24 -0300

    Creates SearchBarView

---
+++ imdb_project/Views/MovieSearchView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieSearchView.swift
+//  imdb_project
+//
+//  Created by Guilherme de Carvalho Correa on 17/05/23.
+//
+
+import SwiftUI
+
+struct MovieSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieSearchView()
+    }
+}
